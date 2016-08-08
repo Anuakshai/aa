5974f4add2da52d55029c2f4db5c5e918d000139
Akshaya Ma <akshai2288@gmail.com>
2016-08-08 16:01:53 +0530

    Add files via upload

---
+++ substrings.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class substrings{
+  public static void main(String args[]){
+    Scanner in=new Scanner(System.in);
+    String s="";
+    System.out.println("enter the string :");
+    s=in.nextLine();
+    if(s.length()%2==0)
+    {
+      System.out.println("the maximum length of non palindrome substring :"+s.length());
+    }
+    else
+    {
+      System.out.println("the maximum length of non palindrome substring is :"+s.length()-1);
+    }
+  }
+}
